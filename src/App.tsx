676939c63bd156034a6a97b6f22bc7193e2e59b0
David Zhou <11278771+DavidZhouNYC@users.noreply.github.com>
2022-08-21 18:39:50 -0400

    Add and initialize Firebase components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import Footer from "./Components/UI/Footer";
 import Header from "./Components/UI/Header";
 import Main from "./Components/UI/Main";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({});
 
 const App = () => {
 	return (
